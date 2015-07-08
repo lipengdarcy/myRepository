13c962344085a0c218109adeecc46be38cdf188e
lipengdarcy <darcy_lee@163.com>
2015-07-08 20:56:21 +0800

    init Ehcache project

---
+++ EhCacheTest/src/main/java/com/hoo/common/ehcache/MethodCacheInterceptor.java
@@ -0,0 +1,120 @@
+/**
+对象缓存就是将查询的数据，添加到缓存中，下次再次查询的时候直接从缓存中获取，
+而不去数据库中查询。
+对象缓存一般是针对方法、类而来的，结合Spring的Aop对象、方法缓存就很简单。这里需要用到切面编程，用到了Spring的MethodInterceptor或是用@Aspect。
+ * */
+
+package com.hoo.common.ehcache;
+
+import java.io.Serializable;
+
+import net.sf.ehcache.Cache;
+
+import net.sf.ehcache.Element;
+
+import org.aopalliance.intercept.MethodInterceptor;
+
+import org.aopalliance.intercept.MethodInvocation;
+
+import org.apache.log4j.Logger;
+
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * 
+ * <b>function:</b> 缓存方法拦截器核心代码
+ * 
+ * @author hoojo
+ * 
+ * @createDate 2012-7-2 下午06:05:34
+ * 
+ * @file MethodCacheInterceptor.java
+ * 
+ * @package com.hoo.common.ehcache
+ * 
+ * @project Ehcache
+ * 
+ * @blog http://blog.csdn.net/IBM_hoojo
+ * 
+ * @email hoojo_@126.com
+ * 
+ * @version 1.0
+ */
+
+public class MethodCacheInterceptor implements MethodInterceptor,
+		InitializingBean {
+
+	private static final Logger log = Logger
+			.getLogger(MethodCacheInterceptor.class);
+	private Cache cache;
+
+	public void setCache(Cache cache) {
+		this.cache = cache;
+	}
+
+	public void afterPropertiesSet() throws Exception {
+		log.info(cache
+				+ " A cache is required. Use setCache(Cache) to provide one.");
+	}
+
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+		String targetName = invocation.getThis().getClass().getName();
+		String methodName = invocation.getMethod().getName();
+		Object[] arguments = invocation.getArguments();
+		Object result;
+
+		String cacheKey = getCacheKey(targetName, methodName, arguments);
+		Element element = null;
+		synchronized (this) {
+			element = cache.get(cacheKey);
+			if (element == null) {
+				log.info(cacheKey + "加入到缓存： " + cache.getName());
+				// 调用实际的方法
+				result = invocation.proceed();
+				element = new Element(cacheKey, (Serializable) result);
+				cache.put(element);
+			} else {
+				log.info(cacheKey + "使用缓存： " + cache.getName());
+			}
+
+		}
+
+		return element.getValue();
+
+	}
+
+	/**
+	 * 
+	 * <b>function:</b> 返回具体的方法全路径名称 参数
+	 * 
+	 * @author hoojo
+	 * 
+	 * @createDate 2012-7-2 下午06:12:39
+	 * 
+	 * @param targetName
+	 *            全路径
+	 * 
+	 * @param methodName
+	 *            方法名称
+	 * 
+	 * @param arguments
+	 *            参数
+	 * 
+	 * @return 完整方法名称
+	 */
+
+	private String getCacheKey(String targetName, String methodName,
+			Object[] arguments) {
+		StringBuffer sb = new StringBuffer();
+		sb.append(targetName).append(".").append(methodName);
+		if ((arguments != null) && (arguments.length != 0)) {
+			for (int i = 0; i < arguments.length; i++) {
+				sb.append(".").append(arguments[i]);
+			}
+		}
+
+		return sb.toString();
+
+	}
+
+}
---
+++ EhCacheTest/src/main/java/com/test/encache/EncacheTest.java
@@ -0,0 +1,61 @@
+
+package com.test.encache;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
+
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author XXX
+ * @version $Id: EncacheTest.java, v 0.1 2014年8月8日 下午5:30:03 XXX Exp $
+ */
+public class EncacheTest {
+	private static final org.slf4j.Logger logger    = LoggerFactory.getLogger(EncacheTest.class);
+    //一些配置参数
+    //private final static String configFileName      = "ehcache.xml";
+    //private final static int    maxEntriesLocalHeap = 1000;
+    private static CacheManager cacheManager;
+    static String               cacheName = "cache1";
+
+    public static void main(String[] args) {
+        ehcacheSetUp();
+
+        ehcacheUse();
+    }
+
+    private static void ehcacheSetUp() {
+        logger.info("Setup ehcache");  
+
+        cacheManager = CacheManager.create();
+
+        //CacheConfiguration configuration = new CacheConfiguration(configFileName,
+        //    maxEntriesLocalHeap);
+
+        //Cache cache = new Cache(configuration);
+        cacheManager.addCache(cacheName);
+
+    }
+
+    private static void ehcacheUse() {
+        Cache cache1 = cacheManager.getCache(cacheName);
+        String key = "key1";
+        String value = "value1";
+
+        writeSomeData(cache1, key, value);
+
+        Element element = readSomeData(cache1, key, value);
+
+        System.out.println(element);
+    }
+
+    private static void writeSomeData(Cache cache, String key, String value) {
+        cache.put(new Element(key, value));
+    }
+
+    private static Element readSomeData(Cache cache, String key, String value) {
+        return cache.get(key);
+    }
+}
