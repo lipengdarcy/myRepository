edee79b5f268970411ad3fc5330c245ec2a68794
darcylee <darcy_lee@163.com>
2015-07-09 15:59:02 +0800

    1

---
+++ EhCacheTest/src/main/java/darcy/proxy/staticproxy/TestCount.java
@@ -0,0 +1,17 @@
+package darcy.proxy.staticproxy;
+
+/**
+ * 测试Count类
+ * 
+ * @author Administrator
+ * 
+ */
+public class TestCount {
+	public static void main(String[] args) {
+		CountImpl countImpl = new CountImpl();
+		CountProxy countProxy = new CountProxy(countImpl);
+		countProxy.updateCount();
+		countProxy.queryCount();
+
+	}
+}
