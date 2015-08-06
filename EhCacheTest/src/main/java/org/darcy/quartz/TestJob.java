1f7c1da7c471620437282ec83f31351a9a06217c
darcylee <darcy_lee@163.com>
2015-08-06 11:19:11 +0800

    quartz

---
+++ EhCacheTest/src/main/java/org/darcy/quartz/TestJob.java
@@ -0,0 +1,23 @@
+package org.darcy.quartz;
+
+//测试工作类
+//TestJob.java
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+public class TestJob implements Job {
+	SimpleDateFormat DateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	Date d = new Date();
+	String returnstr = DateFormat.format(d);
+
+	public void execute(JobExecutionContext arg0) throws JobExecutionException {
+		// TODO Auto-generated method stub
+		System.out.println(returnstr + "★★★★★★★★★★★");
+	}
+
+}
