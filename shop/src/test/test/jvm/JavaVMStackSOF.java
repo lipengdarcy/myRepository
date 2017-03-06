38e2d7531a7b3832b1395617b336c8929c8dac82
lipengdarcy <darcy_lee@163.com>
2017-03-06 11:14:27 +0800

    11

---
+++ shop/src/test/test/jvm/JavaVMStackSOF.java
@@ -0,0 +1,17 @@
+package test.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * -Xss128k
+ * -Xss参数减少栈内存容量
+ * */
+public class JavaVMStackSOF {
+	private int stackLength = 1;
+
+	public static void main(String[] args) {
+		
+	}
+
+}
